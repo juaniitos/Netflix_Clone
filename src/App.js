764b3ae0eb01da3873ff7f8f2f2f12cc55c5381c
Juan Solórzano <juan.solorzano19@gmail.com>
2024-07-03 17:14:04 -0500

    Updated index.css and fixed footer position

--- src/App.js
+++ src/App.js
@@ -9,26 +9,30 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            <>
-              <WelcomeSection />
-              <MovieList />
-            </>
-          } 
-        />
-        <Route path="/movies" element={<MovieList />} />
-        <Route path="/movie/:id" element={<MovieDetail />} />
-        <Route path="/series" element={<div>Series Component</div>} />
-        <Route path="/people" element={<div>People Component</div>} />
-        <Route path="/trending" element={<div>Trending Component</div>} />
-        <Route path="/top-rated" element={<div>Top Rated Component</div>} />
-        <Route path="/upcoming" element={<div>Upcoming Component</div>} />
-      </Routes>
-      <Footer />
+      <div >
+        <NavBar />
+        <div className="main-content">
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <>
+                <WelcomeSection />
+                <MovieList />
+              </>
+            } 
+          />
+          <Route path="/movies" element={<MovieList />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/series" element={<div>Series Component</div>} />
+          <Route path="/people" element={<div>People Component</div>} />
+          <Route path="/trending" element={<div>Trending Component</div>} />
+          <Route path="/top-rated" element={<div>Top Rated Component</div>} />
+          <Route path="/upcoming" element={<div>Upcoming Component</div>} />
+        </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
